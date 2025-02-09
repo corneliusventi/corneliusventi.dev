01b30646efdc67cef238bb7590dbb95b2f30424b
Cornelius Venti <corneliusventi@gmail.com>
2025-02-09 14:24:31 +0700

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://corneliusventi.dev",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://corneliusventi.dev/skills",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://corneliusventi.dev/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://corneliusventi.dev/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://corneliusventi.dev/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
